fe82ebc9f4aec9d0648a55ea43c7f3ebba8ae6f9
chrisfranz <chrisfranz@mac.com>
2019-03-11 20:07:32 -0700

    private blockchain

--- Course_Blockchain_Data/Practice_Private_Blockchain_Code_Session/simpleChain.js
+++ Course_Blockchain_Data/Practice_Private_Blockchain_Code_Session/simpleChain.js
@@ -9,45 +9,45 @@ const SHA256 = require('crypto-js/sha256');
 |  Class with a constructor for block 			   |
 |  ===============================================*/
 
-class Block{
-	constructor(data){
-     this.hash = "",
-     this.height = 0,
-     this.body = data,
-     this.time = 0,
-     this.previousBlockHash = ""
-    }
+class Block {
+  constructor(data) {
+    this.hash = "",
+    this.height = 0,
+    this.body = data,
+    this.time = 0,
+    this.previousBlockHash = ""
+  }
 }
 
 /* ===== Blockchain Class ==========================
 |  Class with a constructor for new blockchain 		|
 |  ================================================*/
 
-class Blockchain{
-  constructor(){
+class Blockchain {
+  constructor() {
     this.chain = [];
     this.addBlock(new Block("First block in the chain - Genesis block"));
   }
 
   // Add new block
-  addBlock(newBlock){
+  addBlock(newBlock) {
     // Block height
     newBlock.height = this.chain.length;
     // UTC timestamp
     newBlock.time = new Date().getTime().toString().slice(0,-3);
     // previous block hash
-    if(this.chain.length>0){
+    if (this.chain.length > 0) {
       newBlock.previousBlockHash = this.chain[this.chain.length-1].hash;
     }
     // Block hash with SHA256 using newBlock and converting to a string
     newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
     // Adding block object to chain
-  	this.chain.push(newBlock);
+    this.chain.push(newBlock);
   }
 
   // Get block height
     getBlockHeight(){
-      return this.chain.length-1;
+      return this.chain.length - 1;
     }
 
     // get block
@@ -67,7 +67,7 @@ class Blockchain{
       // generate block hash
       let validBlockHash = SHA256(JSON.stringify(block)).toString();
       // Compare
-      if (blockHash===validBlockHash) {
+      if (blockHash === validBlockHash) {
           return true;
         } else {
           console.log('Block #'+blockHeight+' invalid hash:\n'+blockHash+'<>'+validBlockHash);
@@ -75,10 +75,10 @@ class Blockchain{
         }
     }
 
-   // Validate blockchain
+    // Validate blockchain
     validateChain(){
       let errorLog = [];
-      for (var i = 0; i < this.chain.length-1; i++) {
+      for (var i = 0; i < this.chain.length - 1; i++) {
         // validate block
         if (!this.validateBlock(i))errorLog.push(i);
         // compare blocks hash link
@@ -96,3 +96,11 @@ class Blockchain{
       }
     }
 }
+
+const myChain = new Blockchain();
+const b2 = new Block('second block bitches')
+const b3 = new Block('third block bitches!')
+myChain.addBlock(b2);
+myChain.addBlock(b3);
+
+console.log(myChain.getBlockHeight());
